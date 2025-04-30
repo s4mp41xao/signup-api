38105d7cb9ee0644d148b04c2b5e677785b6174f
s4mp41xao <samuelpaixao@proton.me>
2025-04-30 16:12:39 -0300

    fix: remove duplicated files

--- src/main/java/com/example/login_auth_api/dto/LoginRequestDTO.java
+++
@@ -1,6 +0,0 @@
-package com.example.login_auth_api.dto;
-
-public record LoginRequestDTO (String email, String password){
-
-
-}
--- src/main/java/com/example/login_auth_api/dto/RegisterRequestDTO.java
+++
@@ -1,4 +0,0 @@
-package com.example.login_auth_api.dto;
-
-public record RegisterRequestDTO(String name, String email, String password) {
-}
